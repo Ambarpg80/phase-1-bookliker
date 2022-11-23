3747c03b969e107f30e8419fa22454c3bb45d62a
Ambar <Ambarpg80@gmail.com>
2022-11-23 16:38:07 -0500

    commit tree 9c0fcb9ad34098a3c5ce131c549ff3f435ca0121

--- js/index.js
+++ js/index.js
@@ -1,1 +1,72 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('http://localhost:3000/books')
+	.then(res => res.json())
+.then((books) =>  booksList(books)) 
+});
+
+
+ /* =========== LIST OF BOOK TITLES ============== */
+function booksList(books){
+    const ul = document.getElementById('list')
+
+    for(let book of books){
+        let li = document.createElement('li');
+        li.innerHTML = book.title ;
+        ul.append(li)
+
+    li.addEventListener('click', ()=>{clickedBook(book)})    
+    }
+}
+
+
+ /* =========== CLICK ON BOOKLIST FOR DETAILED INFO ============= */
+function clickedBook(book){
+    const bookDetail = document.querySelector('#show-panel')
+
+    if(book){
+        //console.log(book)
+        let img = document.createElement('img');
+        const description = document.createElement('p')
+        const btn = document.createElement('button')
+        bookDetail.innerHTML = '';
+        img.src = `${book.img_url}` ; 
+        description.innerHTML = book.description;
+        //console.log(book['users'])
+        bookDetail.append(img, description)
+        book['users'].forEach(bookUser => {
+            const userlist = document.createElement('p')
+            userlist.innerText = bookUser['username'];
+            bookDetail.append(userlist)
+        })
+        btn.innerHTML= `<img src="book.png">`
+        btn.id = "like-book"
+        btn.type = 'submit'
+        bookDetail.append(btn)
+
+        btn.addEventListener('click' , () =>{likeABook(book)})
+        }
+    }
+
+
+     /* =========== UPDATE USERS IN DB ============= */
+function likeABook(book){
+fetch(`http://localhost:3000/books/${book.id}`,{
+        method : "PATCH",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+            },
+        body : JSON.stringify(addAUserlike(book))
+        })
+    .then(res => res.json())
+   .then(book  => clickedBook(book))
+   
+    }
+
+    /* =========== ADD USER TO USERLIST ============ */
+function addAUserlike(book){ 
+   book["users"].push({"id" : 1,
+       "username": "pouros"}) 
+       return(book)
+    }
+   
